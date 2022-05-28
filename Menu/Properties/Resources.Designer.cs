275c95fe8aac3931b90b359f27f157c5e93fe086
Stav Shay <stavsy@mta.ac.il>
2022-05-28 18:39:42 +0300

    first click has done

--- Menu/Properties/Resources.Designer.cs
+++ Menu/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Menu.Properties {
+namespace Ui.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Menu.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Menu.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Ui.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
